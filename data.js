f81faa17cbf64c3e57293d6d7158c913e1896c5e
hammam <hammambenyounes4@gmail.com>
2022-04-17 13:28:47 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,53 @@
+import bcrypt from "bcryptjs";
+
+const data = {
+  users: [
+    {
+      name: "Basir",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+      isSeller: true,
+      seller: {
+        name: "Puma",
+        logo: "/images/logo1.png",
+        description: "best seller",
+        rating: 4.5,
+        numReviews: 120,
+      },
+    },
+    {
+      name: "John",
+      email: "user@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      discount: 10,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      name: "Adidas Fit Shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 100,
+      discount: 0,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+export default data;
